12f49bd16e9db9384cecabbaac2fd031fd722986
Irving Morales <irvingfisica@gmail.com>
2020-06-09 11:42:25 -0500

    Código funcionando

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,14 @@ fn main() {
 		.read_line(&mut frase)
 		.expect("No pude leer tu frase");
 
-	println!("Tu frase es {}",frase);
+	println!("Tu frase es: {}",frase);
 
 	let caracteres = frase.chars();
 	let mut palabras: Vec<String> = Vec::new();
 
-	println!("{:?}",caracteres);
-
 	let mut inicio = 0;
 
 	for (i,car) in caracteres.enumerate() {
-
-		println!("{},{}",i,car );
 
 		if (car == ' ') | (car == '\r') | (car == '\n') {
 
@@ -34,6 +30,42 @@ fn main() {
 
 	}
 
-	println!("{:?}",palabras);
+	println!("Las palabras son: {:?}",palabras);
 
+	let mut nueva_frase = String::from("");
+
+	for palabra in &palabras {
+
+		let nueva_palabra = match &palabra.chars().nth(0) {
+			Some('A') => pigger_vowel(&palabra),
+			Some('E') => pigger_vowel(&palabra),
+			Some('I') => pigger_vowel(&palabra),
+			Some('O') => pigger_vowel(&palabra),
+			Some('U') => pigger_vowel(&palabra),
+			Some('a') => pigger_vowel(&palabra),
+			Some('e') => pigger_vowel(&palabra),
+			Some('i') => pigger_vowel(&palabra),
+			Some('o') => pigger_vowel(&palabra),
+			Some('u') => pigger_vowel(&palabra),
+			_ => pigger_not_vowel(&palabra),
+		};
+
+		if nueva_frase != "" {
+			nueva_frase = nueva_frase + " " + &nueva_palabra;
+		} else {
+			nueva_frase = nueva_frase + &nueva_palabra;
+		}
+
+	}
+
+	println!("Tu frase piggeada es: {:?}",nueva_frase);
+
+}
+
+fn pigger_vowel(cadena: &String) -> String {
+	cadena[..].to_string() + "-hay"
+}
+
+fn pigger_not_vowel(cadena: &String) -> String {
+	cadena[1..].to_string() + "-" + &cadena[..1] + "ay"
 }
